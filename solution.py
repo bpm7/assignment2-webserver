0d0296ade6a987d0e0d55c80e01e80fdd8c2fb54
Brian Murphy <98722378+bpm7@users.noreply.github.com>
2022-02-19 12:56:47 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -9,41 +9,45 @@ def webServer(port=13331):
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
+  serverSocket.listen(1)
 
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
+        # print(message)
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
-        
+
+        # print(f)
+        outputdata = f.read()#Fill in start     #Fill in end
+        print(outputdata)
         #Send one HTTP header line into socket.
-        #Fill in start
-
-        #Fill in end
-
+        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        connectionSocket.send('Content-Type: text/html\n\n'.encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
+          print('On line: '+str(i))
+          connectionSocket.send(outputdata[i].encode('utf-8'))
 
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
+        print("Ran into an error")        
         # Send response message for file not found (404)
         #Fill in start
-
+        connectionSocket.send('HTTP/1.1 404 Not Found \r\n'.encode())
         #Fill in end
-
 
         #Close client socket
         #Fill in start
+        connectionSocket.close()
 
         #Fill in end
 
